2c68a819280fe5c291fd07b90ecdf138da1283e4
Muhammed-Y-E <erdogan.muhammed.y@gmail.com>
2025-02-21 17:49:37 +0100

    Several Animations work now

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let x = 0;
+let frame = 0;
+let state = 'IDLE'
 
-
+setInterval(moveCharacter, 75);
 
 function moveCharacter() {
-    pirate.src = `img/2/2_entity_000_IDLE_00${x}.png`;
-    x++;
+    pirate.src = `img/2/2_entity_000_${state}_00${frame}.png`;
+    frame++;
+
+
+    if(frame == 7) {
+        frame = 0;
+    }
+}
+
+
+function setState(newState) {
+    frame = 0;
+    state = newState;
 }
