d2401a0c916a28f8598165c0ed83bf7c3b6d3a63
Muhammed-Y-E <erdogan.muhammed.y@gmail.com>
2025-02-23 17:15:42 +0100

    Added Enemys in container, they dont have constant distance

--- script.js
+++ script.js
@@ -3,49 +3,54 @@ let state = 'IDLE';
 let left = 300;
 let leftArrow = false;
 let rightArrow = false;
-let attacking = false; // Gibt an, ob der Angriff gerade stattfindet
+let attacking = false;
+const enemies = [];
+const enemyCount = 3;
 
 
-setInterval(moveCharacter, 75);
+setInterval(moveCharacter,75);
 setInterval(updateGame, 1000 / 60);
 document.onkeydown = checkKey;
 document.onkeyup = unCheckKey;
+createEnemies();
 
 function checkKey(e) {
-
     e = e || window.event;
 
     if (e.keyCode == '37') {
-        // left arrow
-        leftArrow = true;
-        setState('WALK');
+       // left arrow
+       leftArrow = true;
+       setState('WALK');
     }
     else if (e.keyCode == '39') {
-        // right arrow
-        rightArrow = true;
-        setState('WALK');
-    }
-    if (e.keyCode == '68') { // 'd' taste 
+       // right arrow
+       rightArrow = true;
+       setState('WALK');
+    } 
+    if (e.keyCode == '68'){ // 'd' Taste 
         attacking = true;
-    }  
+    }
 }
-
 
 function unCheckKey(e) {
     e = e || window.event;
 
     if (e.keyCode == '37') {
         leftArrow = false;
-    }
-    else if (e.keyCode == '39') {
+    } else if (e.keyCode == '39') {
         rightArrow = false;
     }
 }
 
-
-
 function updateGame() {
     currentBackground.style.objectPosition = `${-left}px`;
+
+        // Update enemy positions to stay fixed on background
+    enemies.forEach(enemy => {
+        enemy.element.style.left = `${enemy.initialX - left}px`;
+    });
+
+
     if(leftArrow) {
         left -= 5;
     }
@@ -55,36 +60,51 @@ function updateGame() {
 
     if(attacking) {
         setState('ATTACK');
-    }
-    else if(leftArrow || rightArrow) {
+    } else if(leftArrow || rightArrow) {
         setState('WALK');
     } else {
         setState('IDLE');
     }
 }
 
-function moveCharacter() {
+function moveCharacter(){
     pirate.src = `img/2/2_entity_000_${state}_00${frame}.png`;
     frame++;
-    if (leftArrow) {
+    if(leftArrow) {
         pirate.style.transform = "scaleX(-1)";
     }
 
-    if (rightArrow) {
+    if(rightArrow) {
         pirate.style.transform = "scaleX(1)";
     }
-  
 
-    if (frame == 7) {
-       attacking = false;
-       frame = 0; 
+    if(frame == 7) {
+        attacking = false;
+        frame = 0;
+    }
+}
+
+
+function createEnemies() {
+    for (let i = 0; i < enemyCount; i++) {
+        const enemy = document.createElement('img');
+        enemy.classList.add('enemy');
+        enemy.src = 'img/Minotaur_01/Minotaur_01_Walking_000.png';
+        enemy.style.left = `${500 + i * 300}px`; // Set different initial X positions
+        document.getElementById('enemiesContainer').appendChild(enemy);
+
+        // Store enemy's position
+        enemies.push({
+            element: enemy,
+            initialX: 500 + i * 300
+        });
     }
 }
 
 
 function setState(newState) {
-    if (state !== newState) {
+    if(state !== newState) {
         frame = 0;
         state = newState;
-    }
+    }  
 }
