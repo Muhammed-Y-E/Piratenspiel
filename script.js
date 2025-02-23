54048a6261cd6acd395193a66d4b72973ef6d01d
Muhammed-Y-E <erdogan.muhammed.y@gmail.com>
2025-02-23 15:13:39 +0100

    Move Character smooth

--- script.js
+++ script.js
@@ -1,44 +1,82 @@
 let frame = 0;
 let state = 'IDLE';
 let left = 300;
+let leftArrow = false;
+let rightArrow = false;
 
-setInterval(moveCharacter,75);
-setInterval(updateBackground, 1000 / 60);
+
+setInterval(moveCharacter, 75);
+setInterval(updateGame, 1000 / 60);
 document.onkeydown = checkKey;
+document.onkeyup = unCheckKey;
 
 function checkKey(e) {
 
     e = e || window.event;
 
     if (e.keyCode == '37') {
-       // left arrow
-       left -= 5;
-       setState('WALK');
+        // left arrow
+        leftArrow = true;
+        setState('WALK');
     }
     else if (e.keyCode == '39') {
-       // right arrow
-       left += 5;
-       setState('WALK');
+        // right arrow
+        rightArrow = true;
+        setState('WALK');
     }
 }
 
-function updateBackground() {
-    currentBackground.style.objectPosition = `${-left}px`;
+
+function unCheckKey(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+        leftArrow = false;
+    }
+    else if (e.keyCode == '39') {
+        rightArrow = false;
+    }
 }
 
-function moveCharacter(){
+
+
+
+function updateGame() {
+    currentBackground.style.objectPosition = `${-left}px`;
+    if(leftArrow) {
+        left -= 5;
+    }
+    if(rightArrow) {
+        left += 5;
+    }
+    if(leftArrow || rightArrow) {
+        setState('WALK');
+    } else {
+        setState('IDLE');
+    }
+}
+
+function moveCharacter() {
     pirate.src = `img/2/2_entity_000_${state}_00${frame}.png`;
     frame++;
+    if (leftArrow) {
+        pirate.style.transform = "scaleX(-1)";
+    }
 
-    if(frame == 7) {
+    if (rightArrow) {
+        pirate.style.transform = "scaleX(1)";
+    }
+  
+
+    if (frame == 7) {
         frame = 0;
     }
 }
 
 
 function setState(newState) {
-    if(state !== newState) {
+    if (state !== newState) {
         frame = 0;
         state = newState;
-    }  
+    }
 }
